69bbcb3dd714e960d86cb7ac95b588970e9e5994
ArtieFuzzz <ArtieFuzzz@users.noreply.github.com>
2022-04-11 11:30:47 +0000

    deploy: d7d223485215083568ee215357056c778da636a8

--- _next/static/4nqfqt9An2y7eeuFTMXne/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/341-0f30bbe9fa5eadc5.js","static/chunks/pages/index-d1b35e3d41b8ba47.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
