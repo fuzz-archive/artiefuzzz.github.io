ea98d7ee483f8a1ebac0226a9319122f8d22ae80
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-04-12 19:30:30 +1000

    More meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import ProfilePicture from '../static/profilepic.png'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -9,6 +10,8 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title> Fuzzy </title>
 
         <meta name='og:title' content='ArtieFuzzz'/>
+        <meta name='theme-color' content='#6e62d1'/>
+        <meta name='og:image' content={ProfilePicture.src}/>
         <meta name='og:description' content='Self Taught Frontend / Backend Developer'/>
         <meta name='description' content='Self Taught Frontend / Backend Developer'/>
         <meta name='og:url' content='https://artiefuzzz.is-a.dev'/>
