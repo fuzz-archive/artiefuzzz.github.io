0e075da97986b577eb32e803d518be4f471a4c91
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-04-13 18:46:39 +1000

    Update 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,11 +6,14 @@ const NotFoundPage: NextPage = () => {
     <>
       <div className='justify-center items-center flex middle text-center'>
         <div className='lg:grid lg:text-center lg:relative mb-5 text-center gap-4'>
-          <h1>Not Found!</h1>
-          <div className='flex items-start mx-auto'>
+          <h1>This page {`doesn't`} exist!</h1>
+          <div className='flex items-start mx-auto space-x-3'>
             <Link href='/'>
-              <a className='btn-round-outline-blue'>Go back Home</a>
+              <a className='btn-round-outline-blue'> Home </a>
             </Link>
+            <a href='#' onClick={() => history.back()} className='btn-round-outline-blue'>
+              Go Back
+            </a>
           </div>
         </div>
       </div>
