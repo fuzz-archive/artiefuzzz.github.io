437f96ebc720d15dfe60efd2d1922547b3968fea
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-04-24 14:37:40 +1000

    Update config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const isProd = process.env.NODE_ENV === "production"
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
-  assetPrefix: isProd ? "/artiefuzzz.github.io/" : "",
   env: {
     storePicturesInWEBP: true,
   }
