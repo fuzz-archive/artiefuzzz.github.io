fe3fddf4357fac6c1ce2eefa33937d90b2912d9e
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-09-22 19:00:04 +1000

    Try to fix the Arc widget

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import Script from 'next/script'
 import ProfilePicture from '../../public/profilepic.png'
 import '../styles/globals.scss'
 
@@ -8,20 +7,26 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title> Artie  </title>
-        <link rel="icon" href="/favicon.png" />
+        <title> Artie </title>
+        <link rel='icon' href='/favicon.png' />
 
-        <meta name='og:title' content='Artie'/>
-        <meta name='theme-color' content='#f9a8d4'/>
-        <meta name='og:image' content={ProfilePicture.src}/>
-        <meta name='og:description' content='Australian Developer doing Frontend and Backend'/>
-        <meta name='description' content='Australian Developer doing Frontend and Backend'/>
-        <meta name='og:url' content='https://artiefuzzz.is-a.dev'/>
-        <meta name='og:site_name' content='Artie'/>
-        <meta name='keywords' content='ArtieFuzzz, Artie, Fuzzz'/>
+        <meta name='og:title' content='Artie' />
+        <meta name='theme-color' content='#f9a8d4' />
+        <meta name='og:image' content={ProfilePicture.src} />
+        <meta
+          name='og:description'
+          content='Australian Developer doing Frontend and Backend'
+        />
+        <meta
+          name='description'
+          content='Australian Developer doing Frontend and Backend'
+        />
+        <meta name='og:url' content='https://artiefuzzz.is-a.dev' />
+        <meta name='og:site_name' content='Artie' />
+        <meta name='keywords' content='ArtieFuzzz, Artie, Fuzzz' />
+
+        <script async src='https://arc.io/widget.min.js#RHQdmiNq' />
       </Head>
-
-      <Script defer={true} src='https://arc.io/widget.min.js#RHQdmiNq'/>
 
       <Component {...pageProps} />
     </>
