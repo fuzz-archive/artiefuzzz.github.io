37945a7c99bd846b30ccdae706538940a147dc6e
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-09-27 11:51:24 +1000

    Migrate to preact

--- next.config.js
+++ next.config.js
@@ -2,11 +2,22 @@ const isProd = process.env.NODE_ENV === "production"
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   poweredByHeader: false,
   env: {
     storePicturesInWEBP: true,
-  }
+  },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      })
+    }
+
+    return config
+  },
 }
 
 module.exports = nextConfig
