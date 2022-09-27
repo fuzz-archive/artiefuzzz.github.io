1d3d866476d57731681d7c20733d64d547901166
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-09-27 22:16:43 +1000

    Update cron again
    I need sleep after this whole mess

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -24,7 +24,7 @@ export default async function handler(
 
       await fetch(process.env.WEBHOOK_URL!, {
         method: FetchMethods.Post,
-        headers: new Headers({ 'Content-Type': 'application/json' }),
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
           embeds: [embed],
         }),
