29547a80188f6d7b20fb334f174a9b7adc7adc98
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-09-27 12:30:26 +1000

    Update NextJS config

--- next.config.js
+++ next.config.js
@@ -7,17 +7,6 @@ const nextConfig = {
   env: {
     storePicturesInWEBP: true,
   },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      })
-    }
-
-    return config
-  },
 }
 
 module.exports = nextConfig
